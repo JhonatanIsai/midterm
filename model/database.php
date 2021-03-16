0e2ffaa85ed7da33a4e775501c9c979fc8d18515
Jhonatan I Medina <62052937+JhonatanIsai@users.noreply.github.com>
2021-03-15 19:15:43 -0700

    added new db credentials

--- model/database.php
+++ model/database.php
@@ -1,12 +1,13 @@
 <?php
     $dsn ="mysql:host=xlf3ljx3beaucz9x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=wpbv8ju2q3a1ag3f";
+  
     $username = 'krvp3c061jjc4618';
     $password = 'v6wfhc56voefhyp8';
 
     
     //Try to connect tot he data base
     try{
-        $db = new PDO($dsn, $username);
+        $db = new PDO($dsn, $username,$password);
     } //If we fail we will display the error page 
     catch(PDOException $usernamee){
         $error_message = "DataBase Errot";
